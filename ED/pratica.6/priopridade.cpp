52ef5efa40a5ae3f48e09369f1df90ca84071cd5
GussAraujo <gussaraujo@outlook.com>
2023-05-03 20:26:07 -0300

    comments

--- ED/pratica.6/priopridade.cpp
+++ ED/pratica.6/priopridade.cpp
@@ -27,21 +27,26 @@ bool desenfileirar(fila *p) {
     obj_t max = 0;
     int posicaoMax = -1;
     int aux = 0;
-    int tam = sizeof(p->itens) / sizeof(p->itens[0]);
 
+    // Se a fila não for vazia
     if (!p->vazia) {
+        // procura o maior valor e seu indice
         for(int i = p->inicio; i != p->fim; i = (i+1) % TAMANHOFILA){
             if(p->itens[i] > max){
                 max = p->itens[i];
                 posicaoMax = i;
             } 
         }
+        // entra se indice for diferente de -1 (0 é um indice válido), ou seja, achou um um 'val'
         if(posicaoMax != -1) {
+            // verifica se a fila só tem 1 elemento e remove ele
             if (posicaoMax == 0 && p->fim == 1) {
                 p->vazia = true;
                 p->cheia = false;
                 p->inicio++;
+            // se tiver mais que 1 elemento
             } else {
+                // vai empurrar os elementos anteriores até chegar no indice encontrado e subsitui-lo tbm
                 for(int i = 0; i < posicaoMax; i++){
                     if(i == 0) {
                         aux = p->itens[i+1];
@@ -93,11 +98,13 @@ int main(){
 
             switch (opcao){
             case 'E':
+                // enfileira o 'val'
                 cin >> val;
                 enfileirar(&p, val);
                 imprimir(&p);
                 break;
             case 'D':
+                // desenfileira o maior 'val' da fila
                 desenfileirar(&p);
                 imprimir(&p);
                 break;
